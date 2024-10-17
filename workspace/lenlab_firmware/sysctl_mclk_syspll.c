159602932ddf358fda265957b6ae4d9e0afe36e4
Christoph Simon <christoph.simon@kit.edu>
2024-10-17 18:23:01 +0200

    setup TICK_TIMER for 100 ms

--- workspace/lenlab_firmware/sysctl_mclk_syspll.c
+++ workspace/lenlab_firmware/sysctl_mclk_syspll.c
@@ -4,18 +4,18 @@ int main(void)
 {
     SYSCFG_DL_init();
 
-    NVIC_EnableIRQ(TIMER_0_INST_INT_IRQN);
+    NVIC_EnableIRQ(TICK_TIMER_INST_INT_IRQN);
 
-    DL_TimerG_startCounter(TIMER_0_INST);
+    DL_TimerG_startCounter(TICK_TIMER_INST);
 
     while (1) {
         __WFI();
     }
 }
 
-void TIMER_0_INST_IRQHandler(void)
+void TICK_TIMER_INST_IRQHandler(void)
 {
-    switch (DL_TimerG_getPendingInterrupt(TIMER_0_INST)) {
+    switch (DL_TimerG_getPendingInterrupt(TICK_TIMER_INST)) {
         case DL_TIMERG_IIDX_ZERO:
             DL_GPIO_togglePins(GPIO_LEDS_PORT, GPIO_LEDS_USER_LED_1_PIN);
             break;
