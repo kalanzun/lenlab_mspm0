d521635e91150d49393f9c403f1469b4522000f9
Christoph Simon <christoph.simon@kit.edu>
2024-10-18 11:13:52 +0200

    pytest fixture probes for launchpad connected, firmware booted or bsl booted

--- tests/test_terminal.py
+++ tests/test_terminal.py
@@ -1,31 +1,11 @@
-from typing import Iterable
+import pytest
 
-from PySide6.QtCore import QIODeviceBase
-from PySide6.QtSerialPort import QSerialPort, QSerialPortInfo
-from pytest import fixture, mark
+from PySide6.QtSerialPort import QSerialPort
 
 
-def find_vid_pid(vid: int, pid: int) -> Iterable[QSerialPortInfo]:
-    for port_info in QSerialPortInfo.availablePorts():
-        if port_info.vendorIdentifier() == vid and port_info.productIdentifier() == pid:
-            yield port_info
-
-
-def connect() -> QSerialPort:
-    port_infos = list(find_vid_pid(0x0451, 0xBEF3))
-    assert len(port_infos) == 2
-
-    aux_port_info, port_info = port_infos
-    port = QSerialPort(port_info)
-    port.setBaudRate(9600)
-
-    assert port.open(QIODeviceBase.OpenModeFlag.ReadWrite)
-    return port
-
-
-@fixture()
-def port() -> QSerialPort:
-    return connect()
+def packet(cmd: bytes):
+    assert len(cmd) == 5
+    return b"L\x01\x00" + cmd
 
 
 class Terminal:
@@ -56,31 +36,23 @@ class Terminal:
         assert reply == packet(b"hello")
 
 
-@fixture()
+@pytest.fixture
 def terminal(port) -> Terminal:
     return Terminal(port)
 
 
-def packet(cmd: bytes):
-    assert len(cmd) == 5
-    return b"L\x01\x00" + cmd
-
-
-@mark.fw
-def test_knock(terminal: Terminal):
+def test_knock(firmware, terminal: Terminal):
     terminal.knock()
 
 
-@mark.fw
-def test_incomplete_packet(terminal: Terminal):
+def test_incomplete_packet(firmware, terminal: Terminal):
     terminal.write(b"L\x05\x00")
     assert not terminal.wait_for_packet()
 
     terminal.knock()
 
 
-@mark.fw
-def test_wrong_baudrate(terminal: Terminal):
+def test_wrong_baudrate(firmware, terminal: Terminal):
     terminal.port.setBaudRate(4_000_000)
     terminal.write(packet(b"knock"))
     assert not terminal.wait_for_packet()
@@ -89,8 +61,7 @@ def test_wrong_baudrate(terminal: Terminal):
     terminal.knock()
 
 
-@mark.fw
-def test_hitchhiker(terminal: Terminal):
+def test_hitchhiker(firmware, terminal: Terminal):
     terminal.write(packet(b"knock") + b"knock")
     assert terminal.wait_for_packet()
     reply = terminal.read()
@@ -101,8 +72,7 @@ def test_hitchhiker(terminal: Terminal):
     terminal.knock()
 
 
-@mark.fw
-def test_reply_too_long(terminal: Terminal):
+def test_reply_too_long(firmware, terminal: Terminal):
     terminal.write(packet(b"get10"))
     assert terminal.wait_for_packet()
     reply = terminal.read()
@@ -113,8 +83,7 @@ def test_reply_too_long(terminal: Terminal):
     terminal.knock()
 
 
-@mark.fw
-def test_baudrate(terminal: Terminal):
+def test_baudrate(firmware, terminal: Terminal):
     terminal.write(packet(b"b4MBd"))
     terminal.wait_for_transmission()
 
@@ -134,16 +103,14 @@ def test_baudrate(terminal: Terminal):
     assert reply == packet(b"b9600")
 
 
-@mark.fw
-def test_probe(terminal: Terminal):
+def test_probe(firmware, terminal: Terminal):
     terminal.write(bytes.fromhex("80 01 00 12 3A 61 44 DE"))
     assert terminal.wait_for_packet()
     reply = terminal.read()
     assert reply == packet(b"hello")
 
 
-@mark.bsl
-def test_probe_bsl(terminal: Terminal):
+def test_probe_bsl(bsl, terminal: Terminal):
     terminal.write(bytes.fromhex("80 01 00 12 3A 61 44 DE"))
     assert terminal.port.waitForReadyRead(200)
     reply = terminal.read()
