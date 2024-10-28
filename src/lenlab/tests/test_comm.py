7115d403eac64b756c529e61fc036e7e0b5ed363
Christoph Simon <christoph.simon@kit.edu>
2024-10-28 19:09:31 +0100

    test checksums

--- src/lenlab/tests/test_comm.py
+++ src/lenlab/tests/test_comm.py
@@ -1,9 +1,26 @@
+from itertools import batched
+
+import pytest
 from PySide6.QtSerialPort import QSerialPort
 
 from lenlab import launchpad
 from lenlab.lenlab import pack
 
 KB = 1024
+
+
+# little endian, reversed polynom
+crc_polynom = 0xEDB88320
+
+
+def crc32(checksum: int = 0xFFFFFFFF):
+    while True:
+        # checksum = checksum ^ value
+        for _ in range(32):
+            mask = -(checksum & 1)
+            checksum = (checksum >> 1) ^ (crc_polynom & mask)
+
+        yield checksum
 
 
 def test_knock(firmware, port: QSerialPort):
@@ -14,10 +31,15 @@ def test_knock(firmware, port: QSerialPort):
     assert reply == launchpad.knock_packet
 
 
+# @pytest.mark.repeat(4096)
 def test_28kb(firmware, port: QSerialPort):
     port.write(pack(b"m28KB"))
     while port.bytesAvailable() < 28 * KB:
-        if not port.waitForReadyRead(300):
+        if not port.waitForReadyRead(100):
             break
     reply = port.readAll().data()
     assert len(reply) == 28 * KB
+
+    for i, (word, checksum) in enumerate(zip(batched(reply[8:], 4), crc32(), strict=False)):
+        value = int.from_bytes(word, "little")
+        assert value == checksum, f"{i=}"
