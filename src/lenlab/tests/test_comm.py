ab87ed8c6bea91b29a211ce3afdc3bd7e1472ac3
Christoph Simon <christoph.simon@kit.edu>
2024-10-28 16:19:33 +0100

    test 28 KB transfer

--- src/lenlab/tests/test_comm.py
+++ src/lenlab/tests/test_comm.py
@@ -1,6 +1,9 @@
 from PySide6.QtSerialPort import QSerialPort
 
 from lenlab import launchpad
+from lenlab.lenlab import pack
+
+KB = 1024
 
 
 def test_knock(firmware, port: QSerialPort):
@@ -9,3 +12,12 @@ def test_knock(firmware, port: QSerialPort):
         assert port.waitForReadyRead(100), "no reply received"
     reply = port.readAll().data()
     assert reply == launchpad.knock_packet
+
+
+def test_28kb(firmware, port: QSerialPort):
+    port.write(pack(b"m28KB"))
+    while port.bytesAvailable() < 28 * KB:
+        if not port.waitForReadyRead(300):
+            break
+    reply = port.readAll().data()
+    assert len(reply) == 28 * KB
