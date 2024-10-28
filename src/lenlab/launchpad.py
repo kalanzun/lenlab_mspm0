913f1bf93e0426ab87096c774b4eed0e2f6591c7
Christoph Simon <christoph.simon@kit.edu>
2024-10-28 11:21:54 +0100

    find_launchpad and lp_plugin for pytest

---
+++ src/lenlab/launchpad.py
@@ -0,0 +1,28 @@
+from PySide6.QtSerialPort import QSerialPortInfo
+
+ti_vid = 0x0451
+ti_pid = 0xBEF3
+
+port_description = "XDS110 Class Application/User UART"
+
+connect_packet = bytes((0x80, 0x01, 0x00, 0x12, 0x3A, 0x61, 0x44, 0xDE))
+knock_packet = b"Lk\x00\x00nock"
+ok_packet = bytes((0x00, 0x08, 0x02, 0x00, 0x3B, 0x06, 0x0D, 0xA7, 0xF7, 0x6B))
+
+
+def find_vid_pid(port_infos: list[QSerialPortInfo], vid: int = ti_vid, pid: int = ti_pid) -> list[QSerialPortInfo]:
+    return [
+        port_info
+        for port_info in port_infos
+        if port_info.vendorIdentifier() == vid and port_info.productIdentifier() == pid
+    ]
+
+
+def find_description(port_infos: list[QSerialPortInfo], description: str = port_description) -> list[QSerialPortInfo]:
+    return [port_info for port_info in port_infos if port_info.description() == description]
+
+
+def find_launchpad(port_infos: list[QSerialPortInfo]) -> list[QSerialPortInfo]:
+    ti_ports = find_vid_pid(port_infos)
+    matches = find_description(ti_ports)
+    return matches if matches else ti_ports
---
+++ src/lenlab/tests/lp_plugin.py
@@ -0,0 +1,86 @@
+from dataclasses import dataclass
+
+import pytest
+from PySide6.QtCore import QIODeviceBase
+from PySide6.QtSerialPort import QSerialPort, QSerialPortInfo
+
+from lenlab.launchpad import find_launchpad
+
+
+def pytest_addoption(parser):
+    # pytest_addoption requires a plugin
+    # pytest imports conftest within a package only after cli parsing
+    parser.addoption(
+        "--port",
+        action="store",
+        help="launchpad serial port name",
+    )
+    parser.addoption(
+        "--fw",
+        action="store_true",
+        default=False,
+        help="assume launchpad with firmware",
+    )
+    parser.addoption(
+        "--bsl",
+        action="store_true",
+        default=False,
+        help="assume launchpad with BSL",
+    )
+
+
+@pytest.fixture(scope="session")
+def port_infos() -> list[QSerialPortInfo]:
+    return QSerialPortInfo.availablePorts()
+
+
+@dataclass(slots=True)
+class Launchpad:
+    port_info: QSerialPortInfo | None = None
+    firmware: bool = False
+    bsl: bool = False
+
+
+@pytest.fixture(scope="session")
+def launchpad(request, port_infos: list[QSerialPortInfo]) -> Launchpad:
+    if port_name := request.config.getoption("--port"):
+        matches = [port_info for port_info in port_infos if port_info.portName() == port_name]
+    else:
+        matches = find_launchpad(port_infos)
+        if len(matches) > 1:
+            pytest.skip("cannot choose port")
+
+    if not matches:
+        pytest.skip("no port found")
+
+    return Launchpad(
+        matches[0],
+        firmware=request.config.getoption("--fw"),
+        bsl=request.config.getoption("--bsl"),
+    )
+
+
+@pytest.fixture(scope="session")
+def firmware(launchpad: Launchpad) -> Launchpad:
+    if launchpad.firmware:
+        return launchpad
+
+    pytest.skip("no firmware found")
+
+
+@pytest.fixture(scope="session")
+def bsl(launchpad: Launchpad) -> Launchpad:
+    if launchpad.bsl:
+        return launchpad
+
+    pytest.skip("no BSL found")
+
+
+@pytest.fixture(scope="module")
+def port(launchpad: Launchpad) -> QSerialPort:
+    port = QSerialPort(launchpad.port_info)
+    if not port.open(QIODeviceBase.OpenModeFlag.ReadWrite):
+        pytest.skip(port.errorString())
+
+    yield port
+    port.close()
