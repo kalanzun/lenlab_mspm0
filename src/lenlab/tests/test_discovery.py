ae26f915adfa428dd21ba9ddac74c1d990cc2e39
Christoph Simon <christoph.simon@kit.edu>
2024-10-31 12:01:29 +0100

    test_discovery shall not import the plugin

--- src/lenlab/tests/test_discovery.py
+++ src/lenlab/tests/test_discovery.py
@@ -7,35 +7,34 @@ from PySide6.QtSerialPort import QSerialPort, QSerialPortInfo
 from lenlab.discovery import Discovery, Future
 from lenlab.launchpad import find_vid_pid
 from lenlab.terminal import Terminal
-from lenlab.tests.lp_plugin import Launchpad
 from lenlab.tests.spy import Spy
 
 logger = getLogger(__name__)
 
 
-def test_all_launchpad_discovery(port_infos: list[QSerialPortInfo]):
+def test_all_launchpad_discovery(launchpad, port_infos: list[QSerialPortInfo]):
     matches = find_vid_pid(port_infos)
     if not matches:
         pytest.skip("no port found")
 
-    result = list(Launchpad.discover(matches))
+    result = list(launchpad.discover(matches))
     if not result:
         pytest.skip("no launchpad found")
 
     assert len(result) == 1
 
 
-def test_single_launchpad_discovery(port_infos: list[QSerialPortInfo]):
+def test_single_launchpad_discovery(launchpad, port_infos: list[QSerialPortInfo]):
     matches = find_vid_pid(port_infos)
     if not matches:
         pytest.skip("no port found")
 
-    result = next(Launchpad.discover(matches), None)
+    result = next(launchpad.discover(matches), None)
     if not result:
         pytest.skip("no launchpad found")
 
 
-def test_firmware_responsive(firmware: Launchpad):
+def test_firmware_responsive(firmware):
     # previous tests have run discovery
     with closing(firmware.open_port()) as port:
         port.write(firmware.knock_packet)
@@ -46,7 +45,7 @@ def test_firmware_responsive(firmware: Launchpad):
         logger.info(f"{firmware.port_name}: firmware responsive")
 
 
-def test_bsl_responsive(bsl: Launchpad):
+def test_bsl_responsive(bsl):
     # previous tests have run discovery
     with closing(bsl.open_port()) as port:
         port.write(bsl.connect_packet)
