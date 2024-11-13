c1d784a54301725c1c8828d1dd60b0a31f00c7ac
Christoph Simon <christoph.simon@kit.edu>
2024-11-13 16:04:57 +0100

    test protocol

---
+++ tests/conftest.py
@@ -0,0 +1,82 @@
+import pytest
+from PySide6.QtCore import QCoreApplication, QIODeviceBase
+from PySide6.QtSerialPort import QSerialPort, QSerialPortInfo
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--fw",
+        action="store_true",
+        default=False,
+        help="run firmware tests",
+    )
+    parser.addoption(
+        "--bsl",
+        action="store_true",
+        default=False,
+        help="run BSL tests",
+    )
+
+
+@pytest.fixture()
+def firmware(request):
+    if not request.config.getoption("fw"):
+        pytest.skip("no firmware")
+
+
+@pytest.fixture()
+def bsl(request):
+    if not request.config.getoption("bsl"):
+        pytest.skip("no BSL")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def app():
+    return QCoreApplication()
+
+
+@pytest.fixture(scope="session")
+def port_infos():
+    return QSerialPortInfo.availablePorts()
+
+
+@pytest.fixture(scope="session")
+def port_info(port_infos):
+    port_description = "XDS110 Class Application/User UART"
+    for port_info in port_infos:
+        if port_info.description() == port_description:
+            return port_info
+
+    pytest.skip("no port")
+
+
+@pytest.fixture(scope="session")
+def port(port_info):
+    port = QSerialPort(port_info)
+    if not port.open(QIODeviceBase.OpenModeFlag.ReadWrite):
+        pytest.skip(port.errorString())
+
+    port.clear()
+    port.setBaudRate(1_000_000)
+
+    yield port
+    port.close()
+
+
+@pytest.fixture(scope="session")
+def send(port):
+    def send(command: bytes):
+        port.write(command)
+
+    return send
+
+
+@pytest.fixture(scope="session")
+def receive(port):
+    def receive(n: int, timeout: int = 100) -> bytes:
+        while port.bytesAvailable() < n:
+            if not port.waitForReadyRead(timeout):
+                raise TimeoutError(f"{port.bytesAvailable()} bytes of {n} bytes received")
+        return port.read(n).data()
+
+    return receive
---
+++ tests/test_protocol.py
@@ -0,0 +1,43 @@
+from PySide6.QtSerialPort import QSerialPort
+
+
+connect_packet = bytes((0x80, 0x01, 0x00, 0x12, 0x3A, 0x61, 0x44, 0xDE))
+knock_packet = b"Lk\x00\x00nock"
+
+
+def test_bsl_resilience_to_false_baud_rate(bsl, port: QSerialPort):
+    # send the knock packet at 1 MBaud
+    port.setBaudRate(1_000_000)
+    port.write(knock_packet)
+    assert not port.waitForReadyRead(100), "BSL should not reply"
+
+    # send the BSL connect packet at 9600 Baud
+    port.setBaudRate(9_600)
+    port.write(connect_packet)
+    assert port.waitForReadyRead(100), "BSL should reply"
+
+    # assume cold BSL
+    # warm BSL for further tests
+    reply = port.readAll().data()
+    assert reply == b"\x00"
+
+
+def test_firmware_resilience_to_false_baud_rate(firmware, port: QSerialPort):
+    # send the BSL connect packet at 9600 Baud
+    port.setBaudRate(9_600)
+    port.write(connect_packet)
+    assert not port.waitForReadyRead(100), "Firmware should not reply"
+
+    # send the knock packet at 1 MBaud
+    port.setBaudRate(1_000_000)
+    port.write(knock_packet)
+    assert port.waitForReadyRead(100), "Firmware should reply"
+
+    reply = port.readAll().data()
+    assert reply == knock_packet
+
+
+def test_knock(firmware, send, receive):
+    send(knock_packet)
+    reply = receive(len(knock_packet))
+    assert reply == knock_packet
