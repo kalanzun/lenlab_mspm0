74ce6652da445145ec97201c4e4e3a0c1b69c413
Christoph Simon <christoph.simon@kit.edu>
2024-12-12 11:09:20 +0100

    new python package lenlab 8.2a1

---
+++ src/lenlab/__main__.py
@@ -0,0 +1,4 @@
+from lenlab import cli
+
+
+cli.main()
---
+++ tests/test_cli.py
@@ -0,0 +1,10 @@
+from lenlab.cli import main
+
+
+def test_cli():
+    main()
+
+
+def test_main():
+    from lenlab import __main__
+    assert __main__
