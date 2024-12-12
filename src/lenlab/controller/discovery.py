95622ae4563781c76cd13cc7e8ab055f806635e2
Christoph Simon <christoph.simon@kit.edu>
2024-12-12 13:18:56 +0100

    discovery.find_launchpad

---
+++ src/lenlab/controller/discovery.py
@@ -0,0 +1,17 @@
+from ..model.launchpad import ti_pid, ti_vid
+from ..model.port_info import PortInfo
+
+
+def find_launchpad(port_infos: list[PortInfo]) -> list[PortInfo]:
+    # vid, pid
+    port_infos = [pi for pi in port_infos if pi.vid_pid == (ti_vid, ti_pid)]
+
+    # cu*
+    if matches := [pi for pi in port_infos if pi.name.startswith("cu")]:
+        port_infos = matches
+
+    # sort by number
+    port_infos.sort(key=lambda pi: pi.sort_key)
+
+    return port_infos
+
