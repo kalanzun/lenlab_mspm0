2690b6f27cf8328212f2d652c860ee5b71ac2406
Christoph Simon <christoph.simon@kit.edu>
2025-01-22 16:53:14 +0100

    logging and creating an error report (save logs to a file)

--- src/lenlab/app/main.py
+++ src/lenlab/app/main.py
@@ -1,15 +1,29 @@
+from importlib import metadata
+from io import StringIO
+from logging import NOTSET, StreamHandler, basicConfig, getLogger
+
 from ..launchpad.discovery import Discovery
 from .app import App
 from .window import MainWindow
+
+logger = getLogger(__name__)
 
 
 def main():
     app = App()
 
+    basicConfig(level=NOTSET)
+    error_report = StringIO()
+    handler = StreamHandler(error_report)
+    getLogger().addHandler(handler)
+
+    version = metadata.version("lenlab")
+    logger.info(f"Lenlab {version}")
+
     discovery = Discovery()
     discovery.available.connect(discovery.probe)
 
-    window = MainWindow(discovery)
+    window = MainWindow(error_report, discovery)
     window.show()
 
     discovery.find()
--- src/lenlab/app/window.py
+++ src/lenlab/app/window.py
@@ -1,15 +1,26 @@
-from PySide6.QtWidgets import QMainWindow, QTabWidget, QVBoxLayout, QWidget
+import io
+
+from PySide6.QtCore import Slot
+from PySide6.QtGui import QAction
+from PySide6.QtWidgets import QFileDialog, QMainWindow, QTabWidget, QVBoxLayout, QWidget
 
 from ..launchpad.discovery import Discovery
+from .bode import BodePlotter
+from .launchpad import LaunchpadWidget
+from .oscilloscope import OscilloscopeWidget
+from .programmer import ProgrammerWidget
 from .status import BoardStatus
+from .voltmeter import VoltmeterWidget
 
 
 class MainWindow(QMainWindow):
-    def __init__(self, discovery: Discovery):
+    def __init__(self, error_report: io.StringIO, discovery: Discovery):
         super().__init__()
 
+        self.error_report = error_report
         self.discovery = discovery
 
+        # widget
         self.board_status = BoardStatus(self.discovery)
 
         tab_widget = QTabWidget()
@@ -27,4 +38,31 @@ class MainWindow(QMainWindow):
         widget.setLayout(layout)
 
         self.setCentralWidget(widget)
+
+        # menu
+        error_report = QAction("Save error report", self)
+        error_report.triggered.connect(self.save_error_report)
+
+        quit_action = QAction("Quit", self)
+        quit_action.triggered.connect(self.close)
+
+        menu_bar = self.menuBar()
+
+        menu = menu_bar.addMenu("&Lenlab")
+        menu.addAction(error_report)
+        menu.addSeparator()
+        menu.addAction(quit_action)
+
+        # title
         self.setWindowTitle("Lenlab")
+
+    @Slot()
+    def save_error_report(self):
+        file_name, file_format = QFileDialog.getSaveFileName(
+            self, "Save error report", "lenlab8-error-report.txt", "TXT (*.txt)"
+        )
+        if not file_name:  # cancelled
+            return
+
+        with open(file_name, "w", encoding="utf-8") as file:
+            file.write(self.error_report.getvalue())
