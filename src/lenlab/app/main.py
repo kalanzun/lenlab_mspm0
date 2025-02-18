9ecae4fb6606b98e5c5c78b13e0fb29a035bce47
Christoph Simon <christoph.simon@kit.edu>
2025-02-18 17:14:03 +0100

    load translations in main

--- src/lenlab/app/main.py
+++ src/lenlab/app/main.py
@@ -3,18 +3,20 @@ import logging
 import sys
 from importlib import metadata
 
-from PySide6.QtCore import QLibraryInfo, QSysInfo
+from PySide6.QtCore import QLibraryInfo, QLocale, QSysInfo, QTranslator
+from PySide6.QtWidgets import QApplication
 
 from ..controller.lenlab import Lenlab
 from ..controller.report import Report
-from .app import App
+from ..language import Language
 from .window import MainWindow
 
 logger = logging.getLogger(__name__)
 
 
 def main(argv: list[str] | None = None) -> None:
-    app = App()
+    app = QApplication()
+
     logging.basicConfig(level=logging.INFO)
 
     parser = argparse.ArgumentParser()
@@ -49,6 +51,16 @@ def main(argv: list[str] | None = None) -> None:
     logger.info(f"Kernel {QSysInfo.prettyProductName()}")
 
     lenlab = Lenlab(args.port, args.probe_timeout, args.reply_timeout)
+
+    # Qt translations
+    path = QLibraryInfo.path(QLibraryInfo.LibraryPath.TranslationsPath)
+    translator = QTranslator(app)
+    if translator.load(QLocale(), "qtbase", "_", path):
+        app.installTranslator(translator)
+
+    # Message translations
+    if QLocale().language() == QLocale.Language.German:
+        Language.language = "german"
 
     window = MainWindow(lenlab, report, rules=sys.platform == "linux")
     window.show()
