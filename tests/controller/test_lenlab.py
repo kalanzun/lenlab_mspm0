21969e17913429b9f241ac9cf4196bb1e2e44329
Christoph Simon <christoph.simon@kit.edu>
2025-02-18 15:52:13 +0100

    test_lenlab

---
+++ tests/controller/test_lenlab.py
@@ -0,0 +1,46 @@
+import pytest
+
+from lenlab.controller.lenlab import Lenlab, Lock
+from lenlab.launchpad.terminal import ResourceError, Terminal
+from lenlab.spy import Spy
+
+
+def test_lock():
+    lock = Lock()
+    assert lock.is_locked
+
+    spy = Spy(lock.locked)
+    assert not lock.acquire()
+    assert spy.count() == 0
+
+    lock.release()
+    assert spy.count() == 1
+    assert not lock.is_locked
+
+    assert lock.acquire()
+    assert spy.count() == 2
+    assert lock.is_locked
+
+
+@pytest.fixture
+def lenlab():
+    return Lenlab("COM0", 400, 400)
+
+
+def test_lenlab(lenlab):
+    assert lenlab
+
+
+def test_on_terminal_ready(lenlab):
+    spy = Spy(lenlab.ready)
+    lenlab.discovery.ready.emit(Terminal("COM0"))
+    assert spy.get_single_arg() is True
+    assert lenlab.lock.is_locked is False
+
+
+def test_on_terminal_error(lenlab):
+    lenlab.discovery.ready.emit(terminal := Terminal("COM0"))
+    spy = Spy(lenlab.ready)
+    terminal.error.emit(ResourceError())
+    assert spy.get_single_arg() is False
+    assert lenlab.lock.is_locked is True
