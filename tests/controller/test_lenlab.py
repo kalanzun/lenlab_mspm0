aaca8bf081bd148f391dbb6a304ab53f5f2b2eab
Christoph Simon <post@christoph-simon.eu>
2025-10-23 17:34:14 +0200

    Tests for lenlab controller

--- tests/controller/test_lenlab.py
+++ tests/controller/test_lenlab.py
@@ -1,6 +1,6 @@
 import pytest
 
-from lenlab.controller.lenlab import Lenlab, Lock
+from lenlab.controller.lenlab import Lenlab, Lock, NoReply
 from lenlab.launchpad.terminal import ResourceError, Terminal
 from lenlab.spy import Spy
 
@@ -44,3 +44,43 @@ def test_on_terminal_error(lenlab):
     terminal.error.emit(ResourceError())
     assert spy.get_single_arg() is False
     assert lenlab.lock.is_locked is True
+
+
+def test_on_reply(lenlab):
+    lenlab.lock.release()
+    lenlab.send_command(b"Lk\x08\x00nock, knock!")
+
+    spy = Spy(lenlab.reply)
+    lenlab.on_reply(b"Lk\x00\x00nock")
+
+    assert spy.get_single_arg() == b"Lk\x00\x00nock"
+    assert lenlab.lock.is_locked is False
+    assert lenlab.timer.isActive() is False
+
+
+def test_on_bsl_reply(lenlab):
+    lenlab.discovery.ready.emit(terminal := Terminal())
+
+    spy = Spy(lenlab.reply)
+    terminal.reply.emit(b"\x00\x08")
+
+    assert spy.count() == 0
+
+
+def test_on_timeout(lenlab):
+    spy = Spy(lenlab.terminal_error)
+    lenlab.timer.timeout.emit()
+
+    assert isinstance(spy.get_single_arg(), NoReply)
+
+
+def test_send_command(lenlab):
+    lenlab.lock.release()
+
+    spy = Spy(lenlab.terminal_write)
+
+    lenlab.send_command(b"Lk\x08\x00nock, knock!")
+    assert spy.get_single_arg() == b"Lk\x08\x00nock, knock!"
+
+    assert lenlab.lock.is_locked is True
+    assert lenlab.timer.isActive() is True
